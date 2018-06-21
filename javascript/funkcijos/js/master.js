9527bec7b9a85e7c4c4478b147c33c1f7261af9c
linasadovske <lina.sadovske@gmail.com>
2018-06-21 21:31:06 +0300

    Po 5 pamokos

---
+++ javascript/funkcijos/js/master.js
@@ -0,0 +1,14 @@
+function addWithReturn(skaicius1, skaicius2) {
+  var suma = skaicius1 + skaicius2;
+  return suma;
+}
+
+function addWithoutReturn(skaicius1, skaicius2) {
+  var suma = skaicius1 + skaicius2;
+  alert(suma);
+}
+
+function(){
+  // Funkcija be pavadinimo vykdoma iškarto užkrovus tinklapį
+  // JOs naudojamos kitose funkcijose
+}
---
+++ javascript/salygos/js/master.js
@@ -0,0 +1,8 @@
+var vardas = prompt("Koks Jūsų vardas?");
+var amzius = prompt('Koks Jūsų amžius?');
+if(amzius < 20){
+  alert('Jūsų negalime prileisti prie svetainės turinio')
+}
+else{
+  alert('Sveiki prisijungę');
+}
