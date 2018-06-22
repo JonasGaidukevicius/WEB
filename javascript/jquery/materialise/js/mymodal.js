5ee6d9d8954a9ab505431d3d9b12298ebbb5adee
linasadovske <lina.sadovske@gmail.com>
2018-06-22 21:41:46 +0300

    po 6 paskaitos

---
+++ javascript/jquery/materialise/js/mymodal.js
@@ -0,0 +1,14 @@
+$(".box p img").click(function () {
+  var mazoPaveiksliukoSrc = $(this).prop("src");
+  var naujasPaveiksliukas = $("<img>");
+  naujasPaveiksliukas.prop("src", mazoPaveiksliukoSrc);
+  $(".mymodalbox").append(naujasPaveiksliukas);
+  $(".mymodal").css({"display":"flex"})
+  // $(".mymodal").show("slow");
+
+})
+
+$(".mymodal").click(function () {
+    $(".mymodal").css({"display":"none"});
+    $(".mymodalbox").empty();
+})
